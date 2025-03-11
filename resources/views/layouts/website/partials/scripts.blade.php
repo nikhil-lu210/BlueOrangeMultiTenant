bb4f79772ce0f521b6de0032fa3ccb403056d2c9
nigelsi2023 <nigel.staffindia@gmail.com>
2025-03-11 18:54:23 +0600

    Notification For Browser

--- resources/views/layouts/website/partials/scripts.blade.php
+++ resources/views/layouts/website/partials/scripts.blade.php
@@ -1,23 +1,28 @@
 <!-- Core JS -->
 <!-- build:js assets/vendor/js/core.js -->
+
+<script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
 <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
+
+<script src="{{ asset('assets/js/custom_js/jquery-confirm/jquery-confirm.min.js') }}"></script>
+<script src="{{ asset('assets/js/custom_js/jquery-confirm/confirm.js') }}"></script>
+
+<script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
 
 <!-- endbuild -->
 
 <!-- Vendors JS -->
-<script src="{{ asset('assets/vendor/libs/nouislider/nouislider.js') }}"></script>
-<script src="{{ asset('assets/vendor/libs/swiper/swiper.js') }}"></script>
-
-<!-- Main JS -->
-<script src="{{ asset('assets/js/front-main.js') }}"></script>
-
-<!-- Page JS -->
-<script src="{{ asset('assets/js/front-page-landing.js') }}"></script>
 
 {{-- Desktop Browser Notification --}}
-{{-- <script src="{{ asset('assets/js/custom_js/notification/browser_notification.js') }}"></script> --}}
+<script>
+    var unreadNotificationsUrl = "{{ url('/notification/get-unread-notifications-for-browser') }}";
+    var markNotificationReadUrl = "{{ url('/notification/mark-as-read-notifications-for-browser/') }}";
+</script>
+<script src="{{ asset('assets/js/custom_js/notification/browser_notification.js') }}"></script>
 
 <!-- Main JS -->
 <script src="{{ asset('assets/js/main.js') }}"></script>
